98fe975df3592d6cdda19d7698ff10b981216e57
Trygve Aspenes <Trygve.Aspenes@met.no>
2016-06-01 09:08:07 +0200

    Working version with calibration

--- mpop/satout/mitiff.py
+++ mpop/satout/mitiff.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2016.
 #from tifffile.tifffile import TIFF_TAGS
 from TiffImagePlugin import IMAGEDESCRIPTION
+from pyparsing import Upcase
 #from libxml2mod import last
 #from aifc import data
 
@@ -94,9 +95,15 @@ def mitiff_writer(filename, root_object, compression=True, mitiff_product_name=N
     tif = TIFF.open(filename, mode ='w')
     #print dir(tif)
     
-    np.set_printoptions(threshold=np.nan)
+    #np.set_printoptions(threshold=np.nan)
     
-    tif.SetField(IMAGEDESCRIPTION, "TEST")
+    #Need to generate information to go into the image description
+    #This is parsed by DIANA to be used when displaying the data
+    image_description = _make_image_description(root_object, product_channels)
+    
+    print str(image_description)
+    #print dtype(image_description)
+    tif.SetField(IMAGEDESCRIPTION, str(image_description))
     for ch in product_channels:
         found_channel = False
         print ch.attrib['name']
@@ -133,6 +140,150 @@ def mitiff_writer(filename, root_object, compression=True, mitiff_product_name=N
 
     tif.close
     
+def _make_image_description(ro, pc):
+    #generate image desdcription for mitiff.
+    """
+     Satellite: NOAA 18
+     Date and Time: 06:58 31/05-2016
+     SatDir: 0
+     Channels:   6 In this file: 1-VIS0.63 2-VIS0.86 3(3B)-IR3.7 4-IR10.8 5-IR11.5 6(3A)-VIS1.6
+     Xsize:  4720
+     Ysize:  5544
+     Map projection: Stereographic
+     Proj string: +proj=stere +lon_0=0 +lat_0=90 +lat_ts=60 +ellps=WGS84 +towgs84=0,0,0 +units=km +x_0=2526000.000000 +y_0=5806000.000000
+     TrueLat: 60 N
+     GridRot: 0
+     Xunit:1000 m Yunit: 1000 m
+     NPX: 0.000000 NPY: 0.000000
+     Ax: 1.000000 Ay: 1.000000 Bx: -2526.000000 By: -262.000000
+
+     Satellite: <satellite name>
+     Date and Time: <HH:MM dd/mm-yyyy>
+     SatDir: 0
+     Channels:   <number of chanels> In this file: <channels names in order>
+     Xsize:  <number of pixels x>
+     Ysize:  <number of pixels y>
+     Map projection: Stereographic
+     Proj string: <proj4 string with +x_0 and +y_0 which is the positive distance from proj origo to the lower left corner of the image data>
+     TrueLat: 60 N
+     GridRot: 0
+     Xunit:1000 m Yunit: 1000 m
+     NPX: 0.000000 NPY: 0.000000
+     Ax: <pixels size x in km> Ay: <pixel size y in km> Bx: <left corner of upper right pixel in km> By: <upper corner of upper right pixel in km>
+     
+     
+     if palette image write special palette
+     if normal channel write table calibration:
+     Table_calibration: <channel name>, <calibration type>, [<unit>], <no of bits of data>, [<calibration values space separated>]\n\n
+    """
+    
+    _image_description = ''
+
+    _image_description += ' Satellite: '
+    if ( ro.satname != None ):
+        _image_description += ro.satname.upper()
+    if ( ro.number != None ):
+        _image_description += ' '+str(ro.number)
+    
+    _image_description += '\n'
+        
+    _image_description += ' Date and Time: '
+    _image_description += ro.time_slot.strftime("%H:%M %d/%m-%Y\n")
+       
+    _image_description += ' SatDir: 0\n'
+    
+    _image_description += ' Channels: '
+    _image_description += str(len(pc))
+    _image_description += ' In this file: '
+    for ch in pc:
+        _image_description += ch.attrib['name-alias']
+        if ch == pc[-1]:
+            _image_description += '\n'
+        else:
+            _image_description += ' '
+       
+    _image_description += ' Xsize: '
+    _image_description += str(ro.area.x_size) + '\n'
+    
+    _image_description += ' Ysize: '
+    _image_description += str(ro.area.y_size) + '\n'
+    
+    _image_description += ' Map projection: Stereographic\n'
+    _image_description += ' Proj string: ' + ro.area.proj4_string
+    if not ("datum","towgs84") in ro.area.proj_dict:
+        _image_description += ' +towgs84=0,0,0'
+
+    if not ("units") in ro.area.proj_dict:
+        _image_description += ' +units=km'
+        
+    
+    _image_description += ' +x_0=%.6f' % (-ro.area.area_extent[0])
+    _image_description += ' +y_0=%.6f' % (-ro.area.area_extent[1])
+    
+    _image_description += '\n'
+    _image_description += ' TrueLat: 60N\n'
+    _image_description += ' GridRot: 0\n'
+    
+    _image_description += ' Xunit:1000 m Yunit: 1000 m\n'
+
+    _image_description += ' NPX: %.6f' % (0)
+    _image_description += ' NPY: %.6f' % (0) + '\n'
+
+    _image_description += ' Ax: %.6f' % (ro.area.pixel_size_x/1000.)
+    _image_description += ' Ay: %.6f' % (ro.area.pixel_size_y/1000.)
+    #But this ads up to upper left corner of upper left pixel.
+    _image_description += ' Bx: %.6f' % (ro.area.area_extent[0]/1000.) #LL_x
+    _image_description += ' By: %.6f' % (ro.area.area_extent[3]/1000.) #UR_y
+    _image_description += '\n'
+    
+    logger.debug("Area extent: ",ro.area.area_extent)
+
+    for ch in pc:        
+        palette=False
+        #Make calibration.
+        if palette:
+            logging.debug("Do palette calibration")
+        else:
+            _image_description += 'Table_calibration: '
+            if 'name-alias' in ch.keys():
+                _image_description += ch.attrib['name-alias']
+            else:
+                _image_description += ch.attrib['name']
+
+            _reverse_offset = 0.;
+            _reverse_scale = 1.;
+
+            if ch.attrib['calibration'] == 'RADIANCE':
+                _image_description += ', Radiance, '
+                _image_description += '[W/m²/µm/sr]'
+                _decimals = 8
+            elif ch.attrib['calibration'] == 'BT':
+                _image_description += ', BT, '
+                _image_description += '[°C]'
+
+                _reverse_offset = 255.;
+                _reverse_scale = -1.;
+                _decimals = 2
+            elif ch.attrib['calibration'] == 'REFLECTANCE':
+                _image_description += ', Reflectance(Albedo), '
+                _image_description += '[%]'
+                _decimals = 2
+
+            else:
+                logger.warning("Unknown calib type. Must be Radiance, Reflectance or BT.")
+
+            _image_description += ', 8, [ '
+            for val in range(0,256):
+                #Comma separated list of values
+                #calib.append(boost::str(boost::format("%.8f ") % (prod_chan_it->min_val + (val * (prod_chan_it->max_val - prod_chan_it->min_val)) / 255.)));
+                _image_description += '{0:.{1}f} '.format((float(ch.attrib['min-val']) + ( (_reverse_offset + _reverse_scale*val) * ( float(ch.attrib['max-val']) - float(ch.attrib['min-val'])))/255.),_decimals)
+                
+            _image_description += ']\n\n'
+
+    
+    return _image_description
+
+
 if __name__ == '__main__':
     from mpop.satellites import PolarFactory
     from mpop.projector import get_area_def
